a97ff1f89e484fdcd8920e12e8cc7f528e678ebb
Adriazola01 <adriazola01@hotmail.com>
2015-06-01 20:56:06 -0600

    Clase4-3

---
+++ ConsoleApplication1/DataBase/numeros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class Numeros
+    {
+        private List<int> list = new List<int>();
+
+        public void Agregar(int numero)
+        {
+            list.Add(numero);
+        }
+
+        public int ObtenerCantidad()
+        {
+            int cantidad = list.Count;
+            //foreach (int i in list)
+            //{
+            //    num++;
+            //}
+            return cantidad;
+        }
+    }
+}
---
+++ ConsoleApplication1/TryCatch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TryCatch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibo = new Fibonacci();
+            int numero = 1;
+            try
+            {
+                while (true)
+                {
+                    fibo.Calcular(numero);
+                    numero++;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(numero);
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+           
+        }
+    }
+}
